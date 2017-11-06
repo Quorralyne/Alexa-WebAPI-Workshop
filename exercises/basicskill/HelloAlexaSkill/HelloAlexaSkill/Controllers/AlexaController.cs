f1e5d0dae5ca5e755d319e5003905c542076d062
Heather Downing <Heather.Downing@vml.com>
2017-11-06 21:46:13 +0100

    Added AlexaController

---
+++ exercises/basicskill/HelloAlexaSkill/HelloAlexaSkill/Controllers/AlexaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloAlexaSkill.Controllers
+{
+    public class AlexaController : ApiController
+    {
+
+        [HttpPost, Route("demo")]
+        public dynamic AlexaDemo(dynamic request)
+        {
+            return new
+            {
+                version = "1.0",
+                sessionAttributes = new { },
+                response = new
+                {
+                    outputSpeech = new
+                    {
+                        type = "PlainText",
+                        text = "Hello, World."
+                    },
+                    card = new
+                    {
+                        type = "Simple",
+                        title = "Hello World",
+                        content = "Hello world!"
+                    },
+                    shouldEndSession = true
+                }
+            };
+        }
+    }
+}
